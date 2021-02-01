bb2789bec690831ccbf9d912e1c2c0641238bc56
Andrew Manzanero <manzanero.andrew@gmail.com>
2021-02-01 12:08:38 -0800

    make heroku happy again

--- app/__init__.py
+++ app/__init__.py
@@ -5,15 +5,11 @@ from app import routes
 from app.auth.routes import auth_bp
 from app.constants import SECRET_KEY, MONGO_URI
 
+connect(host=MONGO_URI)
 
-def create_app():
-    # create and configure the app
-    app = Flask(__name__)
-    app.register_blueprint(routes.bp)
-    app.register_blueprint(auth_bp)
+app = Flask(__name__)
+app.register_blueprint(routes.bp)
+app.register_blueprint(auth_bp)
 
-    app.config["SECRET_KEY"] = SECRET_KEY
-    JWTManager(app)
-
-    connect(host=MONGO_URI)
-    return app
+app.config["SECRET_KEY"] = SECRET_KEY
+JWTManager(app)
