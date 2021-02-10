d958a36d97e70f610c4123e581318f98ef7b1df5
Joses Galdamez <josesgaldamez@Josess-MacBook-Pro.local>
2021-02-10 01:21:18 -0800

    Addded routes for signup and login

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+import json
 
 bp = Blueprint('root', __name__)
 
@@ -9,3 +10,28 @@ def index():
 @bp.route('/<name>', methods=["GET"])
 def hello_name(name):
     return { "message": "Hello, {0}!".format(name) }
+
+#Playing with routing calls Source: https://stackoverflow.com/questions/10434599/get-the-data-received-in-a-flask-request
+
+#Login route
+@bp.route('/auth/login',methods=["POST"])
+def login():
+    data = request.json
+    return jsonify(data)
+
+#Signup route
+@bp.route('/auth/signup',methods=["POST"])
+def signup():
+    data = request.json
+    return jsonify(data)
+
+#validate e-mail route
+@bp.route('/auth/validateEmail', methods=["POST"])
+def validate():
+    data = request.json
+    if data:
+        #do some validation here for existing emails later.
+        if (data['email'] == 'jgaldamez102@gmail.com'):
+            return "used"
+        else:
+            return "unused"
