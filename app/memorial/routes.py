c92c3f50a26a7afa740a4e7cfd252cb22f76ae76
Andrew Manzanero <manzanero.andrew@gmail.com>
2021-02-16 12:02:18 -0800

    update models and routes

--- app/memorial/routes.py
+++ app/memorial/routes.py
@@ -1,40 +1,51 @@
-from flask import Blueprint, request, jsonify
-from flask_jwt_extended import jwt_required
-from app.memorial import controllers
+from flask import Blueprint, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.memorial.controllers import display
+from app.memorial.controllers.create import create
 
-memorial_bp = Blueprint('memorial', __name__)
+memorial_bp = Blueprint('memorial', __name__, url_prefix="/memorials")
 
-
-@memorial_bp.route('/memorial/<creator_email>', methods=['GET'])
+@memorial_bp.route('/<memorial_id>', methods=['GET'])
 @jwt_required
-def memorials(creator_email):
-    response, code = controllers.display(creator_email)
+def memorials(memorial_id):
+    user_id = get_jwt_identity()
+    response, code = display.display_single_memorial_for_user(user_id, memorial_id)
 
     if code != 200:
-        return jsonify({"msg": response}), code
+        return {"msg": response}, code
 
-        # Todo: Need to figure out authentication requirements
+    memorial, role= response
 
-    return jsonify({"memorial": response}), 200
+    return {"memorial": memorial, "role": role}, 200
 
-@memorial_bp.route('/memorial', methods=["POST"])
+@memorial_bp.route('', methods=['GET', 'POST'])
 @jwt_required
-def create_memorials():
-    if not request.is_json:
-        return {"msg": "Missing JSON in request"}, 400
+def memorial():
+    if request.method == "GET":
+        user_id = get_jwt_identity()
+        response, code = display.display_all_user_memorials(user_id)
 
-    kwargs = {
-        "creator_email": request.json.get("creator_email", None),
-        "first_name": request.json.get("first_name", None),
-        "last_name": request.json.get( "last_name", None),
-        "description": request.json.get("description", None),
-    }
+        if code != 200:
+            return {"msg": response}, code
 
-    response, code = controllers.create(**kwargs)
+        memorials, roles = response
 
-    if code != 200:
-        return jsonify({"msg": response}), code
+        return {"memorials": memorials, "roles": roles}, 200
 
-    # Todo: Need to figure out authentication requirements
+    else:
+        if not request.is_json:
+            return {"msg": "Missing JSON in request"}, 400
 
-    return jsonify({"memorial": response}), 200
+        kwargs = {
+            "creator_id": get_jwt_identity(),
+            "first_name": request.json.get("firstName", None),
+            "last_name": request.json.get("lastName", None),
+            "description": request.json.get("description", None),
+        }
+
+        response, code = create(**kwargs)
+
+        if code != 200:
+            return {"msg": response}, code
+
+        return {"memorial": response}, 200
