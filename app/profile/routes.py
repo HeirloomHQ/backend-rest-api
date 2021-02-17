ccc5a754ccd7b732da55175e025773be8e1026f8
Mark Kyaw <annonymous10101@gmail.com>
2021-02-17 01:45:01 -0800

    rm account creation

--- app/profile/routes.py
+++ app/profile/routes.py
@@ -13,25 +13,3 @@ def user(user_email):
         return jsonify({"msg": response}), code
 
     return jsonify({"user": response}), 200
-
-# need to decide route for creating account (?)
-@user_bp.route('', methods=["POST"])
-@jwt_required
-def create_account():
-    if not request.is_json:
-        return {"msg": "Missing JSON in request"}, 400
-
-    kwargs = {
-        "email": request.json.get("email", None),
-        "first_name": request.json.get("first_name", None),
-        "last_name": request.json.get("last_name", None),
-        "password_hash": request.json.get("password_hash", None),
-        "salt": request.json.get("salt", None),
-    }
-
-    response, code = controllers.create(**kwargs)
-
-    if code != 200:
-        return jsonify({"msg", response}), code
-
-    return jsonify({"user": response}), 200
