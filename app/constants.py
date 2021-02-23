a16501974f83adecc91f2f4a12fe9424832af5bf
Andrew Manzanero <manzanero.andrew@gmail.com>
2021-02-22 22:43:08 -0800

    initial email stuff

--- app/constants.py
+++ app/constants.py
@@ -1,12 +1,18 @@
-import os
 from dotenv import load_dotenv
+import os
+from pathlib import Path
 
 IS_PROD = os.environ.get("FLASK_ENV") == "production"
 
 if not IS_PROD:
     load_dotenv()
+    sendgrid_file = Path("../sendgrid.env").resolve()
+    load_dotenv(sendgrid_file)
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 MONGO_URI = os.environ.get("MONGO_URI")
+
+SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
+FROM_EMAIL = 'amanzane@usc.edu'
 
 MAX_REFRESH = 60 * 60 * 24 * 31 # valid for 31 days
--- app/memorial/controllers/create.py
+++ app/memorial/controllers/create.py
@@ -1,4 +1,7 @@
 from app.memorial.repos import MemorialRepo, RoleRepo, RoleTypes
+from app.auth.utils import is_email
+from app.auth.repos import UserRepo
+from app.services import email as email_service, permission
 
 
 def create(creator_id, description, first_name, last_name):
@@ -33,3 +36,25 @@ def create(creator_id, description, first_name, last_name):
     RoleRepo.create(**role)
 
     return created_memorial.to_json(), 201
+
+
+def create_invite(inviter_id, memorial_id, emails):
+    # arg validation
+    for email in emails:
+        if not is_email(email):
+            return "{} is not a proper email".format(email), 400
+
+    role = RoleRepo.get_by_user_id(inviter_id)
+    memorial = MemorialRepo.get_by_id(memorial_id)
+
+    allowed = permission.can_user_execute(permission.Action.MANAGE, role, memorial)
+
+    if not allowed:
+        return "User cannot send invites", 403
+
+    # create invites here
+
+    user = UserRepo.get_user_by_id(inviter_id)
+    email_service.send_invitation_email(emails, user.first_name, "link")
+
+    return "Invites sent", 202
