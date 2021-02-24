c5c6cb913b6a8d32a42ae48fecd140c737acd15e
Mark Kyaw <annonymous10101@gmail.com>
2021-02-23 23:28:35 -0800

    wip routes

--- app/memorial/controllers/edit.py
+++ app/memorial/controllers/edit.py
@@ -1,3 +1,36 @@
+from app.memorial.repos import MemorialRepo, RoleRepo
+from app.services.permission import Action, can_user_execute
 
-def edit_memorial(creator_id, description, first_name, last_name):
+
+def edit_memorial(user_id, memorial_id, first_name, last_name, description, born, died, bio, home_town, cover_photo, page_theme):
+    # 1. Check valid user
+    # 2. Display all user memorials based on user_id
+    # 3. Actual memorial_id
+    # 4. Change memorial based on user_id and memorial_id
+    # 5. Change the content based on memorial
+
+    if not user_id:
+        return {"msg": "Missing user"}, 400
+
+    memorial = MemorialRepo.get_by_id(memorial_id)
+
+    new_memorial = {"description": description,
+                "first_name": first_name,
+                "last_name": last_name,
+                "born": born, "died": died,
+                "bio": bio, "home_town": home_town,
+                "cover_photo": cover_photo,
+                "page_theme": page_theme
+                }
+
+    for field, value in new_memorial.items():
+        if value:
+            memorial[field] = value
+
+    memorial.save()
+
+    return memorial.to_json(), 200
+
+
+def edit_memorial_settings():
     pass
--- app/memorial/routes.py
+++ app/memorial/routes.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, request
 from flask_jwt_extended import jwt_required, jwt_optional, get_jwt_identity
-from app.memorial.controllers import display
+from app.memorial.controllers import display, edit
 from app.memorial.controllers.create import create
 
 memorial_bp = Blueprint('memorial', __name__, url_prefix="/memorials")
+
 
 @memorial_bp.route('/<memorial_id>', methods=['GET'])
 @jwt_optional
@@ -14,9 +15,10 @@ def memorials(memorial_id):
     if code != 200:
         return {"msg": response}, code
 
-    memorial, role= response
+    memorial, role = response
 
     return {"memorial": memorial, "role": role}, 200
+
 
 @memorial_bp.route('', methods=['GET', 'POST'])
 @jwt_required
@@ -55,3 +57,26 @@ def memorial():
             return {"msg": response}, code
 
         return {"memorial": response}, 200
+
+
+@memorial_bp.route('/<memorial_id>/settings', methods=['PUT'])
+@jwt_required
+def settings(memorial_id):
+    user_id = get_jwt_identity()
+    kwargs = {
+        "first_name": request.json.get("firstName", None),
+        "last_name": request.json.get("lastName", None),
+        "description": request.json.get("description", None),
+        "born": request.json.get("born", None),
+        "died": request.json.get("died", None),
+        "bio": request.json.get("bio", None),
+        "home_town": request.json.get("homeTown", None),
+        "cover_photo": request.json.get("coverPhoto", None),
+        "page_theme": request.json.get("pageTheme", None),
+    }
+    response, code = edit.edit_memorial(user_id, memorial_id, **kwargs)
+
+    if code != 200:
+        return {"msg": response}, code
+
+    return {"memorial": response}, 200
