67e847ad64e58b2e56b55cbe29a10a01454b06ef
Andrew Manzanero <manzanero.andrew@gmail.com>
2021-03-26 22:59:07 -0700

    integrate aws

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,20 @@
 from flask import Blueprint, request
+from flask_jwt_extended import jwt_required
 from app.memorial.utils import allowed_file
-from app.constants import ALLOWED_EXTENSIONS, IMG_TMP_UPLOAD
-from os import path
+from app.constants import ALLOWED_EXTENSIONS
+from app.services import aws
 
 
 bp = Blueprint('root', __name__)
+
 
 @bp.route('/', methods=["GET"])
 def index():
     return { "message": "Hello, World!" }
 
+
 @bp.route('/image', methods=['POST'])
+@jwt_required()
 def upload():
     if "img" not in request.files:
         return {"msg": "image was not present in body"}, 400
@@ -21,9 +25,10 @@ def upload():
     if not allowed_file(file.filename):
         return {"msg": "img must be of type: " + str(ALLOWED_EXTENSIONS)}, 400
 
-    # save file to temp folder
-    img_url = path.join(IMG_TMP_UPLOAD, file.filename)
-    file.save(img_url)
+    # save to aws
+    img_url = aws.upload_file(file.filename, file)
+    if img_url is None:
+        return {"msg": "server error"}, 500
 
     return {"imageURL": img_url}, 201
 
